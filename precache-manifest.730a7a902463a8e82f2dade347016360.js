67c1c0f49879af3f6b8dec0aea3bf98f21bb67f1
k_maryam27 <k_maryam27@yahoo.com>
2019-08-03 02:43:34 -0400

    Updates

---
+++ precache-manifest.730a7a902463a8e82f2dade347016360.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf83e779bd6b55d1237f8e16184bebc",
+    "url": "/Assignment-16-simple-react-redux/index.html"
+  },
+  {
+    "revision": "cdb33879d8101eec9336",
+    "url": "/Assignment-16-simple-react-redux/static/css/main.8d7b6f6b.chunk.css"
+  },
+  {
+    "revision": "70e6e6d260db6352113c",
+    "url": "/Assignment-16-simple-react-redux/static/js/2.31579dd0.chunk.js"
+  },
+  {
+    "revision": "cdb33879d8101eec9336",
+    "url": "/Assignment-16-simple-react-redux/static/js/main.9b406c14.chunk.js"
+  },
+  {
+    "revision": "f361af674bcc2c1c3265",
+    "url": "/Assignment-16-simple-react-redux/static/js/runtime~main.b9c2359b.js"
+  }
+]);
